72821076022bf5ccc62aca1106c1926664b067b8
Daniel Lobato García <elobato2005@hotmail.com>
2011-07-07 20:14:15 +0200

    Added tests for validation of data and YAML fixtures

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,8 +1,9 @@
 class Product < ActiveRecord::Base
   validates :title, :description, :image_url, :presence => true 
   validates :title, :uniqueness => true
-  validates :image_url :format => {:with
-                                    => %r{\.(gif|jpg|png)$}i,
-                                    :message => 'must be a URL for GIF, JPG or PNG image.'}
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image.'
+  }
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
 end
